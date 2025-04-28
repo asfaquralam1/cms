d4904ca0328668ee1750b70a267a4d26192425eb
unknown <ahsansagor449@gmail.com>
2025-04-28 22:32:46 +0600

    change in updated categories option

--- admin/categories.php
+++ admin/categories.php
@@ -50,9 +50,19 @@
                                 ?>
                                 <input value="<?php if(isset($cat_title)) {echo $cat_title;} ?>" type="text" class="form-control" name="cat_title">
                                 <?php  } }?>
+
+                                <!-- Update Query -->
+                                <?php
+                                if (isset($_POST['update_category'])) {
+                                    $the_cat_title = $_POST['cat_title'];
+                                    $query = "UPDATE categories SET cat_title = '{$the_cat_title}' WHERE cat_id = '{$the_cat_id}'";
+                                    $update_query = mysqli_query($conn, $query);
+                                    header("Location: categories.php");
+                                 }
+                                ?>
                                 </div>
                                 <div class="form-group">
-                                    <input class="btn btn-primary" type="submit" name="submit" value="Update Category">
+                                    <input class="btn btn-primary" type="submit" name="update_category" value="Update Category">
                                 </div>
                             </form>
                         </div>
