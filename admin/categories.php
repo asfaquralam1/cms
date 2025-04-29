8e140409a21eb1d510ebec82c420c0aa7993019c
asfaquralam1 <ahsansagor449@gmail.com>
2025-04-29 17:58:48 +0600

    change in update categories

--- admin/categories.php
+++ admin/categories.php
@@ -8,7 +8,6 @@
             <div class="col-lg-10">
                 <div class="row">
                     <div class="col-lg-6">
-                        <div class="form-group">
                             <?php
                             if (isset($_POST['submit'])) {
                                 $cat_title = $_POST['cat_title'];
@@ -26,7 +25,7 @@
                                 }
                             }
                             ?>
-                            <form action="" method="post">
+                             <form action="" method="post">
                                 <div class="form-group">
                                     <label for="">Add Category</label>
                                     <input type="text" class="form-control" name="cat_title">
@@ -35,37 +34,12 @@
                                     <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
                                 </div>
                             </form>
-
-                            <form action="" method="post">
-                                <div class="form-group">
-                                    <label for="">Edit Category</label>
-                                    <?php
-                                 if (isset($_GET['edit'])) {
-                                    $cat_id = $_GET['edit'];
-                                    $query = "SELECT * from categories WHERE cat_id = $cat_id";
-                                    $selected_category_id = mysqli_query($conn, $query);
-                                    while ($row = mysqli_fetch_assoc($selected_category_id)) {
-                                        $cat_id = $row['cat_id'];
-                                        $cat_title = $row['cat_title'];
-                                ?>
-                                <input value="<?php if(isset($cat_title)) {echo $cat_title;} ?>" type="text" class="form-control" name="cat_title">
-                                <?php  } }?>
-
-                                <!-- Update Query -->
-                                <?php
-                                if (isset($_POST['update_category'])) {
-                                    $the_cat_title = $_POST['cat_title'];
-                                    $query = "UPDATE categories SET cat_title = '{$the_cat_title}' WHERE cat_id = '{$the_cat_id}'";
-                                    $update_query = mysqli_query($conn, $query);
-                                    header("Location: categories.php");
-                                 }
-                                ?>
-                                </div>
-                                <div class="form-group">
-                                    <input class="btn btn-primary" type="submit" name="update_category" value="Update Category">
-                                </div>
-                            </form>
-                        </div>
+                      <?php
+                      if(isset($_GET['edit'])) {
+                          $cat_id = $_GET['edit'];
+                          include "admin/update_categories.php";
+                      }
+                      ?>
                     </div>
                     <div class="col-lg-6">
                         <table>
@@ -87,8 +61,8 @@
                                     echo "<tr>";
                                     echo "<td>{$cat_id}</td>";
                                     echo "<td>{$cat_title}</td>";
-                                    echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
-                                    echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
+                                    echo "<td><a href='admin/categories.php?delete={$cat_id}'>Delete</a></td>";
+                                    echo "<td><a href='admin/acategories.php?edit={$cat_id}'>Edit</a></td>";
                                     echo "</tr>";
                                 }
                                 ?>
@@ -103,7 +77,7 @@
                             </tbody>
                         </table>
                     </div>
-                </div>
+                 </div>
             </div>
         </div>
     </div>
